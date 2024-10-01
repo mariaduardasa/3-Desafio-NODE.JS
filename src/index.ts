9f380a238ac9a07c56aaacb5b4b257121562d199
Maria Eduarda de Sá <maria.sa1@estudante.ifgoiano.edu.br>
2024-10-01 15:18:06 -0300

    feat: initialize API with basic server setup and folder structure

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { urlencoded } from 'body-parser';
+
+
+const app = express();
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send('funcionou')
+})
+
